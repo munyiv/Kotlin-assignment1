6a29919e9d0aabefc09624a7200aeeec675887d5
student <audreymunyiva@gmail.com>
2021-02-20 20:36:50 +0300

    first commit

---
+++ src/Student records.kt
@@ -0,0 +1,11 @@
+fun main() {var name= ("audrey munyiva")
+    var age:Byte=19
+    var phoneNumber= "0795860190"
+    var weight:Float=55.5.toFloat()
+    var citizen:String=("kenyan")
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
+}
